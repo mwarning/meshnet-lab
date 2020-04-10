08b2d4b7dff2c1e29375d36d6d3407013d8c3219
Moritz Warning <moritzwarning@web.de>
2020-04-10 17:20:45 +0200

    plot value range instead of standard error

--- tests/tools/csv_merge.py
+++ tests/tools/csv_merge.py
@@ -14,7 +14,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('input', help='Input CSV file')
 parser.add_argument('output', help='Output CSV file')
 parser.add_argument('--span', type=int, required=True, help='Amount of consecutive rows to be combined into a mean value.')
-parser.add_argument('--column', nargs='+', required=True, help='Calculate a standard deviation from this column and add it to the next column. Column numbering starts at 1. If a header title is present, the new columns name will be suffixed with "_sd".')
+parser.add_argument('--type', type=str, required=True, choices=['sd', 'se', 'vr'], help='Calculate a standard deviation, standard error or value range. Column title suffix is "_sd", "_se" or "_vr".')
+parser.add_argument('--columns', nargs='+', required=True, help='Calculate a standard deviation. Column title prefix is "_sd".')
 
 args = parser.parse_args()
 
@@ -41,6 +42,20 @@ def calc_standard_deviation(values):
 	for value in values:
 		d.append((value - mean) * (value - mean))
 	return math.sqrt((1.0 / (len(d) - 1)) * sum(d))
+
+def calc_standard_error(values):
+	return calc_standard_deviation(values) / math.sqrt(len(values))
+
+def calc_value_range(values):
+	min = None
+	max = None
+	for value in values:
+		if min == None or min > value:
+			min = value
+		if max == None or max < value:
+			max = value
+
+	return abs(max - min)
 
 def get_row_list(lines, col):
 	row = []
@@ -76,14 +91,22 @@ Round a value to the given decimal places, e.g.:
 def format_float(value, places):
 	return str(round(value, places)).rstrip('0').rstrip('.')
 
-def handle_rows(rows, columns):
+def handle_rows(rows, columns, type):
 	output_row = []
 	for col in range(0, len(rows[0])):
 		if (col + 1) in columns:
 			values = get_row_list(rows, col)
 			places = get_decimal_places(rows, col)
 			output_row.append(format_float(calc_mean(values), places))
-			output_row.append(format_float(calc_standard_deviation(values), places))
+			if type == 'sd':
+				output_row.append(format_float(calc_standard_deviation(values), places))
+			elif type == 'se':
+				output_row.append(format_float(calc_standard_error(values), places))
+			elif type == 'vr':
+				output_row.append(format_float(calc_value_range(values), places))
+			else:
+				print('unknown type: {}'.format(type))
+				exit(1)
 		elif is_float(rows[0][col]):
 			# other columns that are numbers => average
 			values = get_row_list(rows, col)
@@ -95,12 +118,12 @@ def handle_rows(rows, columns):
 
 	return output_row
 
-def handle_header(header, columns):
+def handle_header(header, columns, type):
 	output_header = []
 	for col in range(0, len(header)):
 		if (col + 1) in columns:
 			output_header.append(header[col])
-			output_header.append(header[col] + '_sd')
+			output_header.append(header[col] + '_' + type)
 		else:
 			output_header.append(header[col])
 
@@ -138,11 +161,11 @@ with open(args.input, 'r') as file:
 
 	if csv.Sniffer().has_header(sample):
 		for line in reader:
-			args.column = translate_columns(line, args.column)
-			output_header = handle_header(line, args.column)
+			args.columns = translate_columns(line, args.columns)
+			output_header = handle_header(line, args.columns, args.type)
 			break
 	else:
-		args.column = translate_columns([], args.column)
+		args.columns = translate_columns([], args.columns)
 
 	rows = []
 	for line in reader:
@@ -150,11 +173,11 @@ with open(args.input, 'r') as file:
 			rows.append(line)
 		else:
 			# process every <span> rows
-			output_rows.append(handle_rows(rows, args.column))
+			output_rows.append(handle_rows(rows, args.columns, args.type))
 			rows = [line]
 
 	if len(rows) == args.span:
-		output_rows.append(handle_rows(rows, args.column))
+		output_rows.append(handle_rows(rows, args.columns, args.type))
 	elif len(rows) != 0:
 		print("Warning: {} lines left => skipped".format(len(rows)))
 
