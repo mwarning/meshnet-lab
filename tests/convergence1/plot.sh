d1e6f6d6939b201b3656f5be3e3c71847a8efd89
Moritz Warning <moritzwarning@web.de>
2020-04-10 02:19:02 +0200

    fix column names

--- tests/convergence1/plot.sh
+++ tests/convergence1/plot.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-for dataid in 'line' 'rtree' 'lattice4'; do
+for id in 'line' 'rtree' 'lattice4'; do
 	gnuplot -e "
-		set title \"Reachability on a $dataid of 100 nodes.\nLinks without packet loss, 100 random pings over 5-6 seconds\";	\
+		set title \"Reachability on a $id of 100 nodes.\nLinks without packet loss, 100 random pings over 5-6 seconds\";	\
 		set grid;												\
 		set term png;											\
 		set terminal png size 1280,960;							\
-		set output 'convergence-$dataid.png';					\
+		set output 'convergence-$id.png';					\
 		set key spacing 3 font 'Helvetica, 18';					\
 		set ylabel 'packets arrived [%]';						\
 		set xlabel 'wait after start [sec]';					\
 		set termoption lw 3;									\
 		set yrange [-5:105];									\
 		plot													\
-		'convergence-none-$dataid.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'none',				\
-		'convergence-batman-adv-$dataid.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'batman-adv',	\
-		'convergence-babel-$dataid.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'babel',			\
-		'convergence-yggdrasil-$dataid.csv' (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'yggdrasil',	\
-		'convergence-olsr2-$dataid.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'olsr2',			\
-		'convergence-bmx6-$dataid.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'bmx6',				\
-		'convergence-bmx7-$dataid.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'bmx7';				\
+		'convergence-none-$id.csv' using (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'none',				\
+		'convergence-batman-adv-$id.csv' using (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'batman-adv',	\
+		'convergence-babel-$id.csv' using (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'babel',			\
+		'convergence-yggdrasil-$id.csv' (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'yggdrasil',	\
+		'convergence-olsr2-$id.csv' using (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'olsr2',			\
+		'convergence-bmx6-$id.csv' using (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'bmx6',				\
+		'convergence-bmx7-$id.csv' using (column('offset_sec')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'bmx7';				\
 	"
 done
