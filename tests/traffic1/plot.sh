388162a79184740f32baf2f1174d6d856a592a38
Moritz Warning <moritzwarning@web.de>
2020-04-15 17:05:16 +0200

    plot tx instead of rx
    rx might come from different sources

--- tests/traffic1/plot.sh
+++ tests/traffic1/plot.sh
@@ -12,31 +12,31 @@ for id in 'line' 'rtree' 'grid4'; do
 		set output '${prefix}traffic1-$id.png';														\
 		set key spacing 1 font 'Helvetica, 12';												\
 		set xlabel '# number of nodes';														\
-		set ylabel 'rx per node [KB/s]';													\
+		set ylabel 'tx per node [KB/s]';													\
 		set y2label 'packet arrival [%]';													\
 		set y2tics 0, 10;																	\
 		set ytics nomirror;																	\
 		set termoption lw 3;																\
 		plot																				\
-		'${prefix}traffic1-batman-adv-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'batman-adv [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-batman-adv-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'batman-adv [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-batman-adv-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'batman-adv [%]' axis x1y2, \
 		\
-		'${prefix}traffic1-babel-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'babel [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-babel-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'babel [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-babel-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'babel [%]' axis x1y2, \
 		\
-		'${prefix}traffic1-yggdrasil-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'yggdrasil [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-yggdrasil-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'yggdrasil [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-yggdrasil-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'yggdrasil [%]' axis x1y2, \
 		\
-		'${prefix}traffic1-olsr2-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'olsr2 [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-olsr2-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'olsr2 [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-olsr2-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'olsr2 [%]' axis x1y2, \
 		\
-		'${prefix}traffic1-bmx6-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'bmx6 [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-bmx6-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'bmx6 [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-bmx6-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'bmx6 [%]' axis x1y2, \
 		\
-		'${prefix}traffic1-bmx7-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'bmx7 [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-bmx7-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'bmx7 [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-bmx7-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'bmx7 [%]' axis x1y2,	\
 		\
-		'${prefix}traffic1-cjdns-$id.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'cjdns [KB/s/node]' axis x1y1, \
+		'${prefix}traffic1-cjdns-$id.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'cjdns [KB/s/node]' axis x1y1, \
 		'${prefix}traffic1-cjdns-$id.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'cjdns [%]' axis x1y2, \
 		;\
 	"
--- tests/traffic2/plot.sh
+++ tests/traffic2/plot.sh
@@ -3,7 +3,7 @@
 # to distinguish multiple runs (if needed)
 prefix="$1"
 
-../tools/csv_merge.py traffic2-batman-adv-grid4.csv traffic2-batman-adv-grid4_new.csv --span 10 --columns-range 'rx_bytes'
+../tools/csv_merge.py traffic2-batman-adv-grid4.csv traffic2-batman-adv-grid4_new.csv --span 10 --columns-range 'tx_bytes'
 
 gnuplot -e "
 	set title \"Traffic for batman-adv on grid4\n1. Start daemons, 2. Wait 300s, 3. Measure for 60s with 300 pings (10 times for mean value and value range)\";	\
@@ -13,14 +13,14 @@ gnuplot -e "
 	set output '${prefix}traffic2-batman-adv-grid4.png';									\
 	set key spacing 3 font 'Helvetica, 18';												\
 	set xlabel '# number of nodes';														\
-	set ylabel 'rx per node [KB/s]';												\
+	set ylabel 'tx per node [KB/s]';												\
 	set y2label 'packet arrival [%]';													\
 	set termoption lw 3;																\
 	set xtics 0, 100;																	\
 	set y2tics 0, 10;																	\
 	set ytics nomirror;																	\
 	plot																				\
-	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')):(((column('rx_bytes_range') / 1000) / (column('duration_ms') / 1000)) / 2) with errorbars title '' axis x1y1,	\
-	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('rx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'rx per node [KB/s]' axis x1y1,					\
+	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')):(((column('tx_bytes_range') / 1000) / (column('duration_ms') / 1000)) / 2) with errorbars title '' axis x1y1,	\
+	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'tx per node [KB/s]' axis x1y1,					\
 	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'pings arrived [%]' axis x1y2;	\
 "
