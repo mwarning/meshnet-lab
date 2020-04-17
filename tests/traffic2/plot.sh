590b1c2a06576f868dcaeee50e693c1dbfba00ab
Moritz Warning <moritzwarning@web.de>
2020-04-17 12:16:07 +0200

    fix traffic1 plot.sh script

--- tests/traffic2/plot.sh
+++ tests/traffic2/plot.sh
@@ -3,7 +3,7 @@
 # to distinguish multiple runs (if needed)
 prefix="$1"
 
-../tools/csv_merge.py traffic2-batman-adv-grid4.csv traffic2-batman-adv-grid4_new.csv --span 10 --columns-range 'tx_bytes'
+../tools/csv_merge.py ${prefix}traffic2-batman-adv-grid4.csv ${prefix}traffic2-batman-adv-grid4_new.csv --span 10 --columns-range 'tx_bytes'
 
 gnuplot -e "
 	set title \"Traffic for batman-adv on grid4\n1. Start daemons, 2. Wait 300s, 3. Measure for 60s with 300 pings (10 times for mean value and value range)\";	\
@@ -14,13 +14,13 @@ gnuplot -e "
 	set key spacing 3 font 'Helvetica, 18';												\
 	set xlabel '# number of nodes';														\
 	set ylabel 'tx per node [KB/s]';												\
-	set y2label 'packet arrival [%]';													\
+	set y2label 'ping arrival [%]';													\
 	set termoption lw 3;																\
 	set xtics 0, 100;																	\
 	set y2tics 0, 10;																	\
 	set ytics nomirror;																	\
 	plot																				\
-	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')):(((column('tx_bytes_range') / 1000) / (column('duration_ms') / 1000)) / 2) with errorbars title '' axis x1y1,	\
+	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')):((((column('tx_bytes_range') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) / 2) with errorbars notitle axis x1y1,	\
 	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(((column('tx_bytes') / 1000) / (column('duration_ms') / 1000)) / column('node_count')) with linespoints title 'tx per node [KB/s]' axis x1y1,					\
-	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with points title 'pings arrived [%]' axis x1y2;	\
+	'${prefix}traffic2-batman-adv-grid4_new.csv' using (column('node_count')):(100 * column('packets_received') / column('packets_send')) with linespoints title 'pings arrived [%]' axis x1y2;	\
 "
