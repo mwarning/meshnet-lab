ea0acc5ee34da5dce370379b8d5edabada390583
Moritz Warning <moritzwarning@web.de>
2020-05-02 16:34:48 +0200

    update freifunk converters

--- data/freifunk/original_freifunk_data/convert_hopglass.py
+++ data/freifunk/original_freifunk_data/convert_hopglass.py
@@ -13,40 +13,64 @@ parser.add_argument('--formatted', action="store_true", help="Formatted JSON out
 args = parser.parse_args()
 
 links = {}
-
 # map to give each node a short number
 nodes = {}
+
+
+def link_id(link):
+	source = link['source']
+	target = link['target']
+	if source > target:
+		return f"{source}->{target}"
+	else:
+		return f"{target}->{source}"
 
 with open(args.input, "r") as file:
 	obj = json.load(file)
 
 	# record all nodes
 	for node in obj['JSON']['rows']:
-		if node['id'] not in nodes:
-			nodes[node['id']] = {'id': len(nodes), 'label': node['id']}
+		e = {'id': len(nodes)}
 
-		if 'links' not in node['value']:
-			continue
+		if 'hostname' in node['value']:
+			e['name'] = node['value']['hostname']
 
-		for lnode in node['value']['links']:
-			if lnode['id'] not in nodes:
-				nodes[lnode['id']] = {'id': str(len(nodes)), 'name': lnode['id']}
+		if 'latlng' in node['value']:
+			e['x'] = float(node['value']['latlng'][0])
+			e['y'] = float(node['value']['latlng'][1])
+
+		nodes[node['id']] = e
+
+		if 'links' in node['value']:
+			for lnode in node['value']['links']:
+				if lnode['id'] not in nodes:
+					e = {'id': len(nodes)}
+
+					if lnode['id'].endswith('.olsr'):
+						e['name'] = lnode['id'][:-5]
+
+					nodes[lnode['id']] = e
 
 	for node in obj['JSON']['rows']:
-		source = node['id']
-
 		if 'links' not in node['value']:
 			continue
 
 		for lnode in node['value']['links']:
 			target = lnode['id']
-			if source > target:
-				link_id = "{}->{}".format(source, target)
-			else:
-				link_id = "{}->{}".format(target, source)
 
-			if link_id not in links:
-				links[link_id] = {'source': nodes[source]['id'], 'target': nodes[target]['id']}
+			link = {
+				'source': nodes[node['id']]['id'],
+				'target': nodes[lnode['id']]['id']
+			}
+
+			if 'olsr_ipv4' in lnode:
+				link_quality = lnode['linkQuality']
+				link['source_tq'] = link_quality
+				link['target_tq'] = link_quality
+
+			lid = link_id(link)
+			if lid not in links:
+				links[lid] = link
 
 if args.formatted:
 	json.dump({'nodes': list(nodes.values()), 'links': list(links.values())}, sys.stdout, indent="  ", sort_keys = True)
--- data/freifunk/original_freifunk_data/convert_meshviewer.py
+++ data/freifunk/original_freifunk_data/convert_meshviewer.py
@@ -12,27 +12,80 @@ parser.add_argument("input", help="Input meshviewer file.")
 parser.add_argument('--formatted', action="store_true", help="Formatted JSON output data.")
 args = parser.parse_args()
 
-links = []
-
 # map to give each node a short number
 nodes = {}
+links = {}
+
+def link_id(link):
+	source = link['source']
+	target = link['target']
+
+	if source > target:
+		return f'{source}=>{target}'
+	else:
+		return f'{target}=>{source}'
 
 with open(args.input, "r") as file:
 	obj = json.load(file)
 
 	for node in obj['nodes']:
-		nodes[node['node_id']] = {'id': str(len(nodes)), 'name': node['hostname']}
+		e = {'id': len(nodes)}
+
+		if 'location' in node:
+			x = node['location'].get('latitude')
+			y = node['location'].get('longitude')
+			if y is not None and x is not None:
+				e['x'] = float(x)
+				e['y'] = float(y)
+
+		if 'hostname' in node:
+			e['name'] = node['hostname']
+
+		nodes[node['node_id']] = e
 
 	for link in obj['links']:
 		source = link['source']
 		target = link['target']
-		source_tq = link['source_tq']
-		target_tq = link['target_tq']
 
-		# TODO: set source_tc and target_tc
-		links.append({'source': nodes[source]['id'], 'target': nodes[target]['id']})
+		e = {
+			'source': nodes[source]['id'],
+			'target': nodes[target]['id'],
+			'source_tq': link['source_tq'],
+			'target_tq': link['target_tq']
+		}
+
+		if 'type' in link:
+			type = link['type']
+			if type == 'vpn':
+				e['type'] = 'vpn'
+			elif type == 'wifi':
+				e['type'] = 'wifi'
+			else:
+				e['type'] = 'other'
+
+		links[link_id(e)] = e
+
+	# add gateway nodes/links
+	for node in obj['nodes']:
+		gateway = node.get('gateway')
+		nexthop = node.get('gateway_nexthop')
+		if gateway is not None and nexthop is not None:
+			gw_node_id = gateway.replace(':', '')
+			# add gateway
+			if gw_node_id not in nodes:
+				nodes[gw_node_id] = {'id': len(nodes), 'name': gw_node_id}
+
+			link = {
+				'source': nodes[gw_node_id]['id'],
+				'target': nodes[node['node_id']]['id'],
+				'type': 'vpn'
+			}
+
+			# add link to gateway
+			if link_id(link) not in links:
+				links[link_id(link)] = link
 
 if args.formatted:
-	json.dump({'nodes': list(nodes.values()), 'links': links}, sys.stdout, indent="  ", sort_keys = True)
+	json.dump({'nodes': list(nodes.values()), 'links': list(links.values())}, sys.stdout, indent="  ", sort_keys = True)
 else:
-	json.dump({'nodes': list(nodes.values()), 'links': links}, sys.stdout, sort_keys = True)
+	json.dump({'nodes': list(nodes.values()), 'links': list(links.values())}, sys.stdout, sort_keys = True)
