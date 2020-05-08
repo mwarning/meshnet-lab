ea2a1982a4534fb4d79758fe6f8bb69ada47073e
Moritz Warning <moritzwarning@web.de>
2020-05-08 22:52:07 +0200

    connectivity1: fix unit

--- tests/connectivity1/plot.sh
+++ tests/connectivity1/plot.sh
@@ -15,7 +15,7 @@ gnuplot -e "
 	set terminal png size 1280,960; \
 	set output '${prefix}connectivity1.png'; \
 	set key spacing 2 font 'Helvetica, 18' center right; \
-	set xlabel 'Connection range [km]'; \
+	set xlabel 'Connection range [m]'; \
 	set ylabel 'connectivity [%]'; \
 	set xtics 50; \
 	set ytics 10; \
