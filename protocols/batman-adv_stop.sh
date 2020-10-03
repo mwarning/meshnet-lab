0e3c4c8cfa46b04992d3b04ecf23219d7bbbcb2e
Moritz Warning <moritzwarning@web.de>
2020-10-03 08:37:33 +0200

    fix batman-adv stop script for clear

--- protocols/batman-adv_stop.sh
+++ protocols/batman-adv_stop.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-batctl meshif "bat0" interface del "uplink" 2> /dev/null
+address="$1"
+id="$2"
+
+if [ -n "$id" ]; then
+  # called for each node
+  batctl meshif "bat0" interface del "uplink" 2> /dev/null
+else
+  # called once per remote
+  for ns in $(ip netns list); do
+    ip netns exec "$ns" batctl meshif "bat0" interface del "uplink" 2> /dev/null
+  done
+fi
+
 true
 
-# destroys every bridge attached to node...
+# destroys every bridge attached to node... bug?
 #modprobe -r batman-adv
