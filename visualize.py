0e8280f65bfed82570687251710be5b572884135
Benjamin Reich <dev@benjaminreich.de>
2021-04-26 12:38:35 +0200

    more versatile function

--- visualize.py
+++ visualize.py
@@ -15,10 +15,11 @@ def draw(file):
         for link in f["links"]:
             dot.edge(str(link["source"]), str(link["target"]))
 
-    print(dot.source)
+    return dot
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("graph_file", type=argparse.FileType('r'), help="Path to the created graph json file")
     args = parser.parse_args()
-    draw(args.graph_file)
+    result = draw(args.graph_file)
+    print(result.source)
