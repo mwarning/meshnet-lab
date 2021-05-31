83fd219a173e791bc699c622c41acdecaa331bee
Benjamin Reich <dev@benjaminreich.de>
2021-05-31 08:48:07 +0200

    more advanced graphviz

--- visualize.py
+++ visualize.py
@@ -9,11 +9,19 @@ def draw(file):
     
     if "nodes" in f:
         for node in f["nodes"]:
-            dot.node(str(node["id"]))
+            if "name" in node:
+                dot.node(str(node["id"]), label=node["name"])
+            else:
+                dot.node(str(node["id"]))
 
     if "links" in f:
         for link in f["links"]:
+            # if "type" in link:
+            #     dot.edge(str(link["source"]), str(link["target"]), label=link["type"])
+            # else:
             dot.edge(str(link["source"]), str(link["target"]))
+
+
 
     return dot
 
