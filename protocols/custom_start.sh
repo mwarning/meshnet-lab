65e7f408008addcf0ee5b4a50c056ac137a836a5
Moritz Warning <moritzwarning@web.de>
2021-07-31 21:47:23 +0200

    custom: fix wrong variables

--- protocols/custom_start.sh
+++ protocols/custom_start.sh
@@ -6,10 +6,10 @@ PROGRAM="/usr/bin/true"
 address="$1"
 id="$2"
 
-echo "start custom mesh on ${remote} in ${id}"
+echo "start custom mesh on ${address} in ${id}"
 
 ip link set "uplink" down
 ip link set "uplink" up
 
 prefix=$(basename "${PROGRAM}")
-${PROGRAM} -a "$PROTOCOL" -d -i "uplink" -s /tmp/${prefix}-${id}.sock -l /tmp/{prefix}-${id}.log
+${PROGRAM} -p "$PROTOCOL" -d -i "uplink" -s /tmp/${prefix}-${id}.sock -l /tmp/${prefix}-${id}.log
