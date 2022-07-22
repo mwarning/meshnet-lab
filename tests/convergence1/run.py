624398fea45bf4207e6827409f866a60c46b23f7
Moritz Warning <moritzwarning@web.de>
2022-07-22 17:39:37 +0200

    convergence1: remove ospf

--- tests/convergence1/run.py
+++ tests/convergence1/run.py
@@ -65,7 +65,7 @@ def run(protocol, files, csvfile):
 		network.clear(remotes)
 
 for file in ['../../data/line/line-0050.json', '../../data/grid4/grid4-0049.json', '../../data/rtree/rtree-0050.json']:
-	for protocol in ['babel', 'batman-adv', 'bmx6', 'bmx7', 'cjdns', 'olsr1', 'olsr2', 'ospf', 'yggdrasil']:
+	for protocol in ['babel', 'batman-adv', 'bmx6', 'bmx7', 'cjdns', 'olsr1', 'olsr2', 'yggdrasil']:
 		name = file.split('/')[-2]
 		with open(f"{prefix}convergence1-{protocol}-{name}.csv", 'w+') as csvfile:
 			run(protocol, file, csvfile)
