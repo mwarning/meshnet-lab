4e117dba0d4998fc32b436107fbde1a2285ee586
Moritz Warning <moritzwarning@web.de>
2024-03-11 18:06:22 +0100

    protcols: simplify batman-adv stop script

--- protocols/batman-adv_stop.sh
+++ protocols/batman-adv_stop.sh
@@ -3,17 +3,5 @@
 address="$1"
 id="$2"
 
-if [ -n "$id" ]; then
-  # called for each node
-  batctl meshif "bat0" interface del "uplink" 2> /dev/null
-else
-  # called once per remote
-  for ns in $(ip netns list); do
-    ip netns exec "$ns" batctl meshif "bat0" interface del "uplink" 2> /dev/null
-  done
-fi
-
+batctl meshif "bat0" interface destroy 2> /dev/null
 true
-
-# destroys every bridge attached to node... bug?
-#modprobe -r batman-adv
