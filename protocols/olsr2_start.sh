859993ab1ef94001e736c45ddf244dc6102183e9
Moritz Warning <moritzwarning@web.de>
2024-05-11 21:55:47 +0200

    olsr2: remove IPv4 setup
    This fixes the protocol setup.

--- protocols/olsr2_start.sh
+++ protocols/olsr2_start.sh
@@ -23,13 +23,6 @@ cat <<- EOF > "/tmp/olsrd2-${id}.conf"
 	bindto  default_accept
 EOF
 
-addr4() {
-  local mac=$(cat "/sys/class/net/$1/address")
-  IFS=':'; set $mac; unset IFS
-  [ "$6" = "ff" -o "$6" = "00" ] && set $1 $2 $3 $4 $5 "01"
-  printf "10.%d.%d.%d" 0x$4 0x$5 0x$6
-}
-
 addr6() {
   local mac=$(cat "/sys/class/net/$1/address")
   IFS=':'; set $mac; unset IFS
@@ -40,7 +33,6 @@ ip -4 addr flush dev "uplink"
 ip -6 addr flush dev "uplink"
 ip link set "uplink" down
 ip link set "uplink" up
-ip a a $(addr4 "uplink")/32 dev "uplink"
 ip a a $(addr6 "uplink")/128 dev "uplink"
 
 olsrd2 "uplink" --load /tmp/olsrd2-${id}.conf
